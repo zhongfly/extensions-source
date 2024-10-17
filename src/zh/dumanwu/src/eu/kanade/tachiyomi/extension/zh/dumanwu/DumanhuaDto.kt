81a8fe405543e0e0b7ecdca18b7c780a87539dfd
zhongfly <11155705+zhongfly@users.noreply.github.com>
2024-10-17 22:56:34 +0800

    Add dumanwu

---
+++ src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/DumanhuaDto.kt
@@ -0,0 +1,31 @@
+package eu.kanade.tachiyomi.extension.zh.dumanwu
+
+import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.Serializable
+
+@Serializable
+class ResponseDto<T>(
+    val code: String = "200",
+    val msg: String = "",
+    val data: T,
+)
+
+@Serializable
+class SearchMangaDto(
+    private val id: String,
+    private val name: String,
+    private val imgurl: String,
+) {
+    fun toSManga() = SManga.create().apply {
+        url = id
+        title = name
+        thumbnail_url = imgurl
+        initialized = true
+    }
+}
+
+@Serializable
+class ChapterDto(
+    val chapterid: String,
+    val chaptername: String,
+)
